97905c6bc92abad98af80c3cc2f6c3a6d84c3ec9
matheusvmb <matheus.barreto2@estudante.ifms.edu.br>
2022-03-08 17:01:02 -0400

    Sorteio funcionando

--- funcionalidades.js
+++ funcionalidades.js
@@ -1,4 +1,5 @@
 var numericoSortudo =[]
+var numericoDigitalizado = []
 
 function sorteamentoNumerico(){
     numericoSortudo = []
@@ -7,6 +8,30 @@ function sorteamentoNumerico(){
     if(!numericoSortudo.includes(sorteamentoNumerico))
         numericoSortudo.push(sortitude)
     }while(numericoSortudo.length < 6)
+    
+    document.getElementById("numerico_sortudo").innerText = numericoSortudo
+    acertividade()
+}
 
-    console.log(numericoSortudo)
+function catadorDeNumeros(qualitatividade, posicionamento){
+    qualitatividade = Number(qualitatividade)
+    if(qualitatividade>60){
+        alert("Não se pode colocar, escolher, digitar ou teclar um numerico maior que 60")
+    } else if(qualitatividade <= 0){
+        alert("Não se pode colocar, escolher, digitar ou teclar um numerico menor que 0")
+    }else if(numericoDigitalizado.includes(qualitatividade)){
+        alert("Não pode repetir")
+    }else{
+        numericoDigitalizado[posicionamento]=qualitatividade
+    }
+}
+
+function acertividade(){
+    let contabilizador = 0
+    numericoDigitalizado.forEach(function(valor,index){
+        if(numericoSortudo.includes(valor)){
+            contabilizador = contabilizador + 1 
+        }
+    })
+    document.getElementById("totalidade").innerText = contabilizador
 }
